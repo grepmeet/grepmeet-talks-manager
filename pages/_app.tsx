44f4ec5242e7ca93458a029a5d34bc734d1c033f
Quentin Machard <quentin.machard@gmail.com>
2022-01-26 19:37:44 +0100

    :lipstick: Configure ChakraUI Theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react';
 
+import theme from '../utils/theme';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
   );
