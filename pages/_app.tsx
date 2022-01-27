b01ece844cc48ba1ec63b01212820352c48bddb5
Quentin Machard <quentin.machard@gmail.com>
2022-01-27 10:30:06 +0100

    :lipstick: Integrate Header component with responsive

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react';
+import type {AppProps} from 'next/app'
+import {ChakraProvider} from '@chakra-ui/react';
 
 import theme from '../utils/theme';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-  );
+import {Header} from "../components/organisms/Header";
+
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <ChakraProvider theme={theme}>
+            <Header/>
+
+            <Component {...pageProps} />
+        </ChakraProvider>
+    );
 }
 
 export default MyApp
